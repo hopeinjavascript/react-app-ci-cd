2c5d4468603534a538e4527837c0e25f612bdb9f
LL\akssood <akshay.sood@indigo.co.in>
2024-02-09 17:34:09 +0530

    refactor: change link text

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React from new react.dev website.
+          Learn React JS and Next JS.
         </a>
       </header>
     </div>
